193e93684791ce22184e2285742f113fc39498ea
Hanzala531 <165744107+Hanzala531@users.noreply.github.com>
2024-07-29 19:47:11 +0500

    Projects of DOM

--- DOM Projects/Project 02/Script.js
+++ DOM Projects/Project 02/Script.js
@@ -5,14 +5,22 @@ form.addEventListener('submit', function(e) {
 
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
+    const guide = document.querySelector('#Weight_guide')
     const results = document.querySelector('#results');
+    const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
 
     if (height === '' || height < 0 || isNaN(height)) {
         results.innerHTML = "Please enter a valid height";
     } else if (weight === '' || weight < 0 || isNaN(weight)) {
         results.innerHTML = "Please enter a valid weight";
     } else {
-        const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
         results.innerHTML = `<span> Your BMI is :  ${bmi}</span>`;
+    }
+    if (bmi < 18.6) {
+        guide.innerHTML = "You are Under Weight";
+    } else if (bmi > 24.9) {
+        guide.innerHTML = "Your are Over weight";
+    } else {
+        guide.innerHTML = "Your BMI is in normal range";
     }
 });
